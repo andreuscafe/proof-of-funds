f5a74a635b8d5861f9f10386608541515b0188cc
Andrés Nicolás Tonello <andresnicolastonello@gmail.com>
2023-04-25 18:35:42 -0300

    Markup and data, styling and responsive done.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,82 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="title" content="Proof of Funds" />
+        <meta
+          name="description"
+          content="A repository to celebrate the work of talented Argentinian designers and showcase it to the world."
+        />
+
+        {/* <!-- Open Graph / Facebook --/> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://argentinianswho.design" />
+        <meta property="og:title" content="Proof of Funds" />
+        <meta
+          property="og:description"
+          content="A repository of talented Argentinian designers."
+        />
+        <meta
+          property="og:image"
+          content="https://argentinianswho.design/img/preview.png"
+        />
+
+        <meta
+          name="theme-color"
+          content="#fff"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#111"
+          media="(prefers-color-scheme: dark)"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta
+          property="og:url"
+          content="https://argentinians-who-design.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Proof of Funds" />
+        <meta
+          property="og:description"
+          content="A repository of talented Argentinian designers."
+        />
+        <meta
+          property="og:image"
+          content="https://argentinianswho.design/img/preview.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="argentinians-who-design.vercel.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://argentinians-who-design.vercel.app/"
+        />
+        <meta name="twitter:title" content="Proof of Funds" />
+        <meta
+          name="twitter:description"
+          content="A repository of talented Argentinian designers."
+        />
+        <meta
+          name="twitter:image"
+          content="https://argentinianswho.design/img/preview.png"
+        />
+      </Head>
+      <body className="bg-black text-[#CECECE]">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,170 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image, { StaticImageData } from "next/image";
+import { Space_Mono, Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+import LemonLogo from "../../public/img/icons/lemon.png";
+import ArgenBtcLogo from "../../public/img/icons/argenbtc.png";
+import BuenBitLogo from "../../public/img/icons/buenbit.png";
+import SatoshiTangoLogo from "../../public/img/icons/satoshitango.png";
+import RipioLogo from "../../public/img/icons/ripio.png";
+import DecryptoLogo from "../../public/img/icons/decrypto.png";
+import LetsBitLogo from "../../public/img/icons/letsbit.png";
+import TiendaCryptoLogo from "../../public/img/icons/tiendacrypto.png";
+import BitsoLogo from "../../public/img/icons/bitso.png";
+import BeloLogo from "../../public/img/icons/belo.png";
+import Link from "next/link";
+
+import { ArrowSvg } from "@/components/ArrowSvg";
+import { CryptoText } from "@/components/CryptoText";
+
+const space_mono = Space_Mono({
+  variable: "--font-space-mono",
+  subsets: ["latin"],
+  weight: "400",
+  style: "normal"
+});
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+  weight: "400",
+  style: "normal"
+});
+
+type ExchangesType = Array<{
+  name: string;
+  iconPath: StaticImageData;
+  url: string;
+  docs: {
+    text: string;
+    url?: string;
+  };
+  last_update?: string;
+  notes?: {
+    text: string;
+    url?: string;
+  };
+}>;
+
+const EXCHANGES: ExchangesType = [
+  {
+    name: "Lemon Cash",
+    iconPath: LemonLogo,
+    url: "https://www.lemon.me/",
+    docs: {
+      text: "Prueba de fondos",
+      url: "https://api.lemoncash.com.ar/api/v1/reserves"
+    },
+    last_update: "Realtime",
+    notes: {
+      text: "Estan trabajando en una versión trustless y real-time",
+      url: "https://medium.com/@limoncit0/transparencia-radical-para-cexs-85aad29d8caf"
+    }
+  },
+  {
+    name: "BuenBit",
+    iconPath: BuenBitLogo,
+    url: "https://www.buenbit.com/",
+    docs: {
+      text: "Prueba de fondos",
+      url: "https://drive.google.com/file/d/1kVSy8-aeO0hLFlB17ZX_x-w7339xpe34/view"
+    },
+    last_update: "11/11/2022",
+    notes: {
+      text: "No muestra address de las wallets"
+    }
+  },
+  {
+    name: "Bitso",
+    iconPath: BitsoLogo,
+    url: "https://bitso.com/",
+    docs: {
+      text: "Post sobre transparencia",
+      url: "https://blog.bitso.com/es-mx/bitso-mx/daniel-vogel-bitso-declaracion-nov-10-2022"
+    },
+    last_update: "10/11/2022",
+    notes: {
+      text: "Dicen estar trabajando en un PoF",
+      url: "https://twitter.com/julian_colombo/status/1594713538722930688?s=61&t=5auPmOJkhMmtSTviaZ9McQ"
+    }
+  },
+  {
+    name: "ArgenBTC",
+    iconPath: ArgenBtcLogo,
+    url: "https://argenbtc.com/",
+    docs: {
+      text: "No presenta"
+    },
+    notes: {
+      text: "Dicen estar trabajando en un PoF",
+      url: "https://twitter.com/argenbtc/status/1595480520716173325?s=61&t=8sALSsLKhwBRtfWwE4GulQ"
+    }
+  },
+  {
+    name: "Belo",
+    iconPath: BeloLogo,
+    url: "https://www.belo.app/",
+    docs: {
+      text: "No presenta"
+    },
+    notes: {
+      text: "Dicen estar trabajando en un PoF",
+      url: "https://twitter.com/locosombrero/status/1593784229619449857?s=61&t=_pfJbg1Qq7rT-oabJ3d9Zg"
+    }
+  },
+  {
+    name: "Decrypto",
+    iconPath: DecryptoLogo,
+    url: "https://www.decrypto.la/",
+    docs: {
+      text: "No presenta"
+    },
+    notes: {
+      text: "Dicen estar trabajando en un PoF",
+      url: "https://twitter.com/jl_palacio/status/1595126400108744705?s=61&t=cwVqNmTaVbXgXFQhfaHQpA"
+    }
+  },
+  {
+    name: "Let's Bit",
+    iconPath: LetsBitLogo,
+    url: "https://letsbit.io/",
+    docs: {
+      text: "No presenta"
+    }
+  },
+  {
+    name: "Ripio",
+    iconPath: RipioLogo,
+    url: "https://www.ripio.com/",
+    docs: {
+      text: "No presenta"
+    }
+  },
+  {
+    name: "SatoshiTango",
+    iconPath: SatoshiTangoLogo,
+    url: "https://www.satoshitango.com/",
+    docs: {
+      text: "No presenta"
+    },
+    notes: {
+      text: "No ofrecen rendimientos pero igual estan trabajando en un PoF",
+      url: "https://twitter.com/satoshitango/status/1595468937659092992?s=61&t=8sALSsLKhwBRtfWwE4GulQ"
+    }
+  },
+  {
+    name: "TiendaCrypto",
+    iconPath: TiendaCryptoLogo,
+    url: "https://tiendacrypto.com/",
+    docs: {
+      text: "No presenta"
+    },
+    notes: {
+      text: "No tienen PoF pero dicen no ofrecer rendimientos",
+      url: "https://twitter.com/tiendacrypto/status/1595107917120016385?s=61&t=cwVqNmTaVbXgXFQhfaHQpA"
+    }
+  }
+];
 
 export default function Home() {
   return (
@@ -14,110 +175,151 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
+      <main
+        className={`font-mono min-h-screen max-w-screen-2xl mx-auto p-5 lg:p-8 pb-12 ${space_mono.variable} ${roboto.variable}`}
+      >
+        <header className=" grid grid-cols-1 lg:grid-cols-12 gap-6 text-[#F5F5F7]">
+          <h1 className="text-2xl col-span-1 lg:col-span-3">PR00F OF FUNDS</h1>
+          <h2 className="col-span-1 lg:col-span-5">
+            <CryptoText
+              hard
+              text="LISTADO DE EXCHANGES ARGENTINOS DE CRIPTOMONEDAS Y SUS INFORMES DE TRANSPARENCIA"
+            />
+          </h2>
+          <a
+            href="https://argentinianswho.design/"
+            target="_blank"
+            className="sol w-[64px] order-first lg:order-last lg:col-start-12 col-span-1"
+          >
+            <div className="normal">
               <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
+                src="/img/sol.svg"
+                alt="Argentina"
+                width={96}
+                height={96}
                 priority
               />
-            </a>
+            </div>
+            <div className="wink">
+              <Image
+                src="/img/sol_guino.svg"
+                alt="Argentina"
+                width={96}
+                height={96}
+                priority
+              />
+            </div>
+          </a>
+        </header>
+
+        <section className="mt-10 lg:mt-36">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mb-4">
+            <span className="text-xs lg:col-span-3 text-[#6E6E73]">
+              Exchange
+            </span>
+            <span className="text-xs col-span-3 text-[#6E6E73] hidden lg:block">
+              Documentos
+            </span>
+            <span className="text-xs col-span-2 text-[#6E6E73] hidden lg:block">
+              Última actualización
+            </span>
+            <span className="text-xs col-span-4 text-[#6E6E73] hidden lg:block">
+              Notas
+            </span>
           </div>
-        </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div className="font-roboto">
+            {EXCHANGES.map((exchange, index) => (
+              <div
+                key={index}
+                className={`grid grid-cols-1 lg:grid-cols-12 gap-4 lg:gap-8 border-b border-[#333333] pt-6 first-of-type:pt-3 pb-6 lg:py-3 items-center text-[#CECECE] lg:hover:bg-[#03030F] hover:text-white transition-colors`}
+              >
+                <Link
+                  href={exchange.url}
+                  target="_blank"
+                  className="lg:col-span-3 flex items-center"
+                >
+                  <Image
+                    src={exchange.iconPath}
+                    alt={exchange.name}
+                    width={32}
+                    height={32}
+                    priority
+                    className="border-[#1D1D1D] border rounded-lg mr-2"
+                  />
+                  <CryptoText text={exchange.name} />
+                </Link>
+
+                <span className="lg:col-span-3 text-[#6E6E73]">
+                  <span className="col-span-3 text-xs text-[#6E6E73] font-mono block lg:hidden mb-2">
+                    Documentos
+                  </span>
+                  {exchange.docs.url ? (
+                    <Link
+                      href={exchange.docs.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="underline text-[#CECECE] flex gap-1"
+                    >
+                      <CryptoText text={exchange.docs.text} />
+                      <ArrowSvg className="w-4 flex-shrink-0" />
+                    </Link>
+                  ) : (
+                    exchange.docs.text
+                  )}
+                </span>
+
+                <span
+                  className={`lg:col-span-2 ${
+                    !exchange.last_update ? "text-[#6E6E73]" : ""
+                  }`}
+                >
+                  <span className="col-span-3 text-xs text-[#6E6E73] font-mono block lg:hidden mb-2">
+                    Última actualización
+                  </span>
+                  {exchange.last_update ? exchange.last_update : "-"}
+                </span>
+
+                <span className="lg:col-span-4 text-[#6E6E73]">
+                  <span className="col-span-3 text-xs text-[#6E6E73] font-mono block lg:hidden mb-2">
+                    Notas
+                  </span>
+                  {exchange.notes ? (
+                    exchange.notes.url ? (
+                      <a
+                        href={exchange.notes.url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="underline text-[#CECECE] flex gap-1"
+                      >
+                        <CryptoText text={exchange.notes.text} />
+                        <ArrowSvg className="w-4 flex-shrink-0" />
+                      </a>
+                    ) : (
+                      exchange.notes.text
+                    )
+                  ) : (
+                    "-"
+                  )}
+                </span>
+              </div>
+            ))}
           </div>
-        </div>
+        </section>
 
-        <div className={styles.grid}>
+        <footer className="mt-12 ">
+          Si queres colaborar con algún dato, escribime a{" "}
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            href="https://twitter.com/f0go"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
+            className="underline text-[#CECECE] inline-flex gap-1"
           >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
+            @f0go
+            <ArrowSvg className="w-4 " />
           </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </footer>
       </main>
     </>
-  )
+  );
 }
